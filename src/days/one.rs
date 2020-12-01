d4ce8be5757cb4478e3a6e2f6c4bcb5076dbfa4d
Sean Kermes <sean@arcbio.com>
2020-12-01 09:15:02 -0500

    Solve day one.
    It took me a little longer to assemble than the triple-nested
    loop that I assume topped the leaderboard, but some hacky
    benchmarking suggests this is noticeably faster, even on this
    pretty small input. Assuming `intersection` is linear time
    (which follows from `contains` being constant time, which I
    think it should be in a HashSet, though the rust docs don't make
    an explicit promise) then part_one should be O(n), and part_two
    should be O(n^2), which nicely take a full power off the naive
    solution.

--- src/days/one.rs
+++ src/days/one.rs
@@ -1,11 +1,55 @@
+use std::collections::HashSet;
+use std::num::ParseIntError;
 use crate::aoc_error::AocError;
 
-pub const NAME: &'static str = "Unknown";
+pub const NAME: &'static str = "Report Repair";
 
-pub fn part_one(_input: &str) -> Result<String, AocError> {
-    Ok("Not implemented".to_string())
+pub fn part_one(input: &str) -> Result<String, AocError> {
+    let nums: HashSet<usize> = input
+        .split('\n')
+        .map(|line| line.parse())
+        .collect::<Result<HashSet<usize>, ParseIntError>>()?;
+
+    let diffs: HashSet<usize> = nums
+        .iter()
+        .map(|n| 2020 - n)
+        .collect();
+
+    let answer: usize = nums
+        .intersection(&diffs)
+        .product();
+
+    Ok(answer.to_string())
 }
 
-pub fn part_two(_input: &str) -> Result<String, AocError> {
-    Ok("Not implemented".to_string())
+pub fn part_two(input: &str) -> Result<String, AocError> {
+    let nums: HashSet<usize> = input
+        .split('\n')
+        .map(|line| line.parse())
+        .collect::<Result<HashSet<usize>, ParseIntError>>()?;
+
+    for num in &nums {
+        let target = 2020 - num;
+        let diffs: HashSet<usize> = nums
+            .iter()
+            .map(|n| if target > *n { target - n } else { 0 })
+            .collect();
+
+        let candidates: Vec<&usize> = nums
+            .intersection(&diffs)
+            .collect();
+
+        if candidates.len() == 2 {
+            let answer: usize = candidates
+                .iter()
+                // For reasons I don't fully understand, `product` is choking
+                // on the type of iter here being &&usize, so I'll just do my
+                // own fold.
+                .fold(*num, |acc, x| acc * *x);
+
+            return Ok(answer.to_string());
+        }
+    }
+
+    Err(AocError::Misc("Didn't find a solution".to_string()))
 }
